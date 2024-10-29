c21601e44c7108d67f04a12bd9fb4dfb558e9017
Vitor Cardoso <vitor.cardoso@merkle.com>
2024-10-29 11:57:50 +0000

    iframe rename

--- blocks/download-center/download-center.js
+++ blocks/download-center/download-center.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
   const link = block.querySelector('a');
   const appUrl = link ? link.getAttribute('href') : block.textContent.trim();
   const iframe = document.createElement('iframe');
-  iframe.src = "https://publish-p139364-e1423304.adobeaemcloud.com/content/celestyal-asset-share/us/en/light.html";
+  iframe.src = appUrl || '';
   iframe.width = '100%';
   iframe.height = '500px';
   iframe.style.border = '3px solid black';
